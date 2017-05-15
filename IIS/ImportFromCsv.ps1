4eee0481f946f035878ff3948c050aa1a0491615
Sam Tomkinson <samrtomkinson@gmail.com>
2017-05-15 22:43:33 +0100

    Added the start of a script to enable windows search if it is disabled

--- IIS/ImportFromCsv.ps1
+++ IIS/ImportFromCsv.ps1
@@ -1,4 +1,7 @@
-$csvAppPools = Import-Csv .\BL_AppPools.csv
-$csvSites = Import-Csv .\BL_Sites.csv
+$sites = Import-Csv .\BL_Sites.csv | Select-Object -Property "path", "applicationPool"
+$csvAppPools = Import-Csv .\BL_AppPools.csv | Where-Object {$sites.path -contains $_.name}
 
-Write-Output $csvSites
+#Write-Output $csvSites
+#Write-Output $csvAppPools
+#Write-Output $sites
+Write-Output $csvAppPools
